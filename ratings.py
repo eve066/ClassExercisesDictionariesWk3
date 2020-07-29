314883ec06de6b4042678f2bdc48fc19e8caab6a
Evelyn Salas <evejs10@gmail.com>
2020-07-29 04:14:37 +0000

    rev restaurant rating with input

--- ratings.py
+++ ratings.py
@@ -10,12 +10,15 @@ for line in get_file:
     rest_score = line[1]
     ratings[rest_name] = int(rest_score)
     
+# for rest_name, rest_score in sorted(ratings.items()):
+# print(f"{rest_name} is rated at {rest_score}.")
+
+
+print("Include your rating for your restaurant")
+rest_name = input("Restaurant name: ")
+rest_score = int(input("Rating: "))
+ratings[rest_name] = int(rest_score)
+
 for rest_name, rest_score in sorted(ratings.items()):
     print(f"{rest_name} is rated at {rest_score}.")
 
-
-
-
-
-
-# put your code here
